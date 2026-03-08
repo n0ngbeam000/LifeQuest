3f8ba8b709109d01ecc0e0d34a02dd95bc4593cd
n0ngbeam000 <n0ngbeam000>
2026-03-08 20:11:14 +0700

    Change database to postgreSQL

--- life_quest/settings.py
+++ life_quest/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'life_quest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lifequest_db',      
+        'USER': 'postgres',          
+        'PASSWORD': os.getenv('DB_PASSWORD'), 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
