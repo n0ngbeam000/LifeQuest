47b117ba0bd54462981c8d8093e1ad039eae6a9c
n0ngbeam000 <n0ngbeam000>
2026-03-17 13:37:12 +0700

    Clean up database config for Render

--- life_quest/settings.py
+++ life_quest/settings.py
@@ -93,27 +93,11 @@ WSGI_APPLICATION = 'life_quest.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
-
-
-if DATABASE_URL and (ON_RAILWAY or 'railway.internal' not in DATABASE_URL):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
